196bf9e0beedf69401dfb8318d584c38caf879a1
i-natalie <natalyaignatenko@gmail.com>
2025-05-04 17:40:16 +0300

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
+export const myEditionDropContractAddress: string = "0x8Dc21067Fefed800e844b2951A3f4DbD54c84037";
 
 // Replace this with your token id
 export const tokenId: string = "0";
